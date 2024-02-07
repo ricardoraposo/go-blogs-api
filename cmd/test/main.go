cf9b5e806250b4c83b6617b7fa305f408eb86139
ricardo <ricardorbxx1@gmail.com>
2024-02-07 18:14:57 -0300

    chore: added some files for testing queries while in development

---
+++ cmd/test/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"log"
+
+	"github.com/ricardoraposo/blogs-api-go/internal/database"
+	"github.com/ricardoraposo/blogs-api-go/internal/entities"
+)
+
+func main() {
+	db := database.NewDatabase()
+
+	accountDB := database.NewUserDB(db.DB)
+
+	user, _ := entities.NewUser("Ricardo", "r@r.com", "123456", "image")
+
+	_, err := accountDB.CreateUser(user)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	users, _ := accountDB.GetUsers()
+
+	for _, u := range users {
+		log.Println(u.DisplayName)
+	}
+}
