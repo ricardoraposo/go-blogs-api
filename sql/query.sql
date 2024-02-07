0d638d46d022c74183618aec0491f07cef729861
ricardo <ricardorbxx1@gmail.com>
2024-02-07 11:04:19 -0300

    fix: decided to go with raw sql instead of generating functions with sqlc

--- sql/query.sql
+++
@@ -1,8 +0,0 @@
--- name: GetUser :one
-select * from users where id = ?;
-
--- name: GetUsers :many
-select * from users;
-
--- name: InsertUser :execresult
-insert into users (display_name, email, password, image) values (?, ?, ?, ?);
