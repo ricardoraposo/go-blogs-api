da9c1afef38a77c2d8e16e47d2d90afdb115f818
ricardo <ricardorbxx1@gmail.com>
2024-02-07 22:14:57 -0300

    feat: added support for http/2.0

--- internal/server/server.go
+++ internal/server/server.go
@@ -3,14 +3,18 @@ package server
 import (
 	"fmt"
 	"net/http"
+
+	"golang.org/x/net/http2"
+	"golang.org/x/net/http2/h2c"
 )
 
 func NewServer(port string) *http.Server {
 	r := NewRouter()
 
+    h2s := &http2.Server{}
 	server := &http.Server{
-		Addr:    fmt.Sprintf(":%s", port),
-		Handler: r,
+		Addr:    fmt.Sprintf("0.0.0.0:%s", port),
+		Handler: h2c.NewHandler(r, h2s),
 	}
 
 	return server
